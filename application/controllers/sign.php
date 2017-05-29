535cef284e6c0b93e4e7e61220d2fd9d539e5bcf
miiftahl <jannaher98@gmail.com>
2017-05-29 11:40:55 +0700

    a

--- application/controllers/sign.php
+++ application/controllers/sign.php
@@ -1,8 +1,34 @@
-<?php
+<?php 
+//session_start(); //we need to start session in order to access it through CI
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sign extends CI_Controller {
+	public function construct(){
+		parent::__construct(); 
+		// Load form helper library 
+		$this->load->helper('form'); 
+		// Load form validation library 
+		$this->load->library('form_validation'); 
+		// Load session library 
+		$this->load->library('session'); 
+		// Load database 
+		$this->load->model('login_database');
+	}
+
+	public function index(){
+		$this->load->view('vlogin');
+	}
+
 	public function do_signup(){
+		// Check validation for user input in SignUp form
+		/*$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('no_hp', 'No_hp', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('sandi', 'Sandi', 'required');
+		if ($this->form_validation->run() == FALSE) {
+			$data['message_display'] = 'Data anda tidak valid!';
+			$this->load->view('vlogin', $data)
+		};*/
 		$nama= $_POST['nama'];
 		$no_hp = $_POST['no_hp'];
 		$email = $_POST['email'];
@@ -13,16 +39,16 @@ class Sign extends CI_Controller {
 			'no_hp' => $no_hp,
 			'email' => $email,
 			'sandi' => $ensandi,//sandi sudah di enkrip
- 			'level' => "admin"
- 		);
-
+ 			'level' => "user"
+			);
 		$res = $this->mymodel->masukkan('pengguna', $data_insert);
-
 		if($res>=1){
-			header("Location: http://localhost/busanakoe/index.php/tampil/login");
+			//$data['message_display'] = 'Registrasi Sukses !';
+			redirect('sign');
 			exit();
 		}else{
-			echo "<h2>Create User Gagal</h2>";
+			//$data['message_display'] = 'Registrasi Gagal !';
+			redirect('sign');
 		}
 	}
 
@@ -31,19 +57,36 @@ class Sign extends CI_Controller {
 		$sandi = $_POST['sandi'];
 		$ensandi = sha1(md5($sandi));
 
-		$data = $this->mymodel->getPengguna($email);
-		$data = get_object_vars($data);
-		$level = $data['level'];
-
+		$res = $this->mymodel->getPengguna($email);
+		$res = get_object_vars($res);
 		$cek = $this->mymodel->login($email, $ensandi);
 		$tes = count($cek);
 
-		if($tes>0 && $level == 'user'){
-			redirect('tampil/keVhomePel/'.$data->nama);
-		}elseif ($tes>0 && $level == 'admin') {
-			redirect('tampil/keVhomeAdm');
+		if($tes>0){
+			$data = array(
+				'nama' => $res['nama'],
+				'no_hp' => $res['no_hp'],
+				'email' => $res['email'],
+				'sandi' => $res['sandi'],
+				'level' => $res['level']
+			);
+			$level = $data['level'];
+
+			$this->session->set_userdata($data);
+
+			if($level == 'user'){
+				redirect('tampil/keVhomePel/');
+			}elseif ($level == 'admin') {
+				redirect('tampil/keVhomeAdm');
+			}
 		}else{
-			redirect('tampil/login');
+			$this->session->set_flashdata('error', 'maaf, username atau password ada yang salah!');
+			redirect('sign');
 		}
+	}
+
+	public funtion logout(){
+		$this->session->sess_destroy();
+		$this->load->view('vlogin');
 	}
 }
